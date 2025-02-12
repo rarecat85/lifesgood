b04ba34be2271e911f3b8710b24e097fbb3d1a94
dope0515 <61546301+dope0515@users.noreply.github.com>
2025-02-12 14:02:46 +0900

    250212 gate페이지 수정
    1. 레이어 팝업 aria 추가
    2. gate.css 간격 수정

--- lifesgood2025_gate/global/src/assets/js/main.js
+++ lifesgood2025_gate/global/src/assets/js/main.js
@@ -439,6 +439,7 @@ document.addEventListener("DOMContentLoaded", function () {
     iframe.innerHTML = `<iframe src="https://www.youtube.com/embed/${youtubeId}?mute=1&enablejsapi=1" enablejsapi="1" title="LG VS Company : CES 2025 Teaser I LG​" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
     // 팝업 활성화
     layerPop.classList.add("active");
+    layerPop.setAttribute("aria-hidden",'false');
     closeBtn.focus();
   });
 
@@ -446,6 +447,7 @@ document.addEventListener("DOMContentLoaded", function () {
   closeBtn.addEventListener("click", function () {
     iframe.innerHTML = "";
     layerPop.classList.remove("active");
+    layerPop.setAttribute("aria-hidden",'true');
     popShowBtn.focus();
   });
 
