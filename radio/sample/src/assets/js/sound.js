81c3f5938188ca807eca4e3e64288a8b951be9ae
RohUkJin <ukjin.roh@concentrix.com>
2025-06-05 16:51:24 +0900

    feature: sound 섹션 진행중

--- radio/sample/src/assets/js/sound.js
+++ radio/sample/src/assets/js/sound.js
@@ -1,84 +1,128 @@
-function soundTitleAnimation() {
-    const title = document.querySelector(".sound-txtbx-title");
-    const titleWrapper = document.querySelector(".sound-txtbx-title-wrapper");
+    const controllerBtn = document.querySelector(".sound-txtbx-controllers-btn");
 
-    const titlerWidth = title.clientWidth;
-    const titleWrapperWidth = titleWrapper.scrollWidth;
+    controllerBtn.addEventListener("click", function () {
+        const isStopped = controllerBtn.getAttribute('aria-pressed');
 
-    const distance = titlerWidth + titleWrapperWidth;
+        console.log(isStopped);
 
-    const speed = 100;
-    const duration = distance / speed;
+        if (isStopped === 'true') {
+            controllerBtn.setAttribute('aria-label', 'play');
+            controllerBtn.setAttribute('aria-pressed', 'false');
 
-    gsap.to(titleWrapper, {
-        x: -distance,
-        ease: "none",
-        duration: duration,
-        repeat: true,
-        repeatDelay: 0,
+            // GSAP LP 회전 시작
+            soundTrackAnimation(0, false)
+        } else {
+            controllerBtn.setAttribute('aria-label', 'pause');
+            controllerBtn.setAttribute('aria-pressed', 'true');
+
+            // GSAP LP 회전 정지
+            soundTrackAnimation(0, true)
+        }
     });
-}
 
-const soundSwiper = document.querySelector(".sound .sound-swiper");
+    function soundTrackAnimation(index = 0, isStop = false) {
+        const track = document.querySelectorAll(".sound-imgbx-track");
 
-if (soundSwiper) {
-    const imgbxs = document.querySelectorAll(".sound .swiper-slide .sound-imgbx");
-    const soundAlbums = document.querySelectorAll(".sound .swiper-slide .sound-imgbx-album");
-    const soundTracks = document.querySelectorAll(".sound .swiper-slide .sound-imgbx-track");
-    const soundTexts = document.querySelectorAll(".sound .swiper-slide .sound-txtbx");
+        trackInstance = gsap.to(track[index], {
+            rotation: 360, // 1분에 몇 도 회전하는지..? 
+            ease: "power3.inOut",
+            // 추후 repeat 제거 / duration(mp3 시간 분량으로)
+            duration: 30,
+            repeat: -1,
+            repeatDelay: 0,
+        });
 
-    const swiperOptions = {
-        slidesPerView: 1,
-        spaceBetween: 16,
-        pagination: {
-            el: ".sound-swiper-controller .swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        touchable: true,
-        // loop: true,
-        on: {
-            init: function () {
-                imgbxs[0].classList.add("active");
-                soundAlbums[0].classList.add("active");
-                soundTracks[0].classList.add("active");
-                soundTexts[0].classList.add("active");
-                soundTitleAnimation();
-            },
-            slideChange: function () {
-                const index_currentSlide = this.realIndex;
-                const currentSlide = this.slides[index_currentSlide];
-
-                imgbxs.forEach(el => el.classList.remove("active"));
-                soundAlbums.forEach(el => el.classList.remove("active"));
-                soundTracks.forEach(el => el.classList.remove("active"));
-                soundTexts.forEach(el => el.classList.remove("active"));
-
-
-                if (currentSlide) {
-                    imgbxs[index_currentSlide].classList.add("active");
-                    soundAlbums[index_currentSlide].classList.add("active");
-                    soundTracks[index_currentSlide].classList.add("active");
-                    soundTexts[index_currentSlide].classList.add("active");
-                    soundTitleAnimation();
-                }
-            }
-        },
-        breakpoints: {
-            769: {
-                slidesPerView: 1.2,
-                spaceBetween: 80,
-            },
-            1441: {
-                slidesPerView: 2.5,
-                spaceBetween: 122,
-            }
-        },
+        if (isStop && trackInstance) {
+            trackInstance.pause();
+        }
     }
 
-    const swiperInstance = new Swiper(soundSwiper, swiperOptions);
+    let trackInstance = null;
 
-}
+    const soundSwiper = document.querySelector(".sound .sound-swiper");
+
+    if (soundSwiper) {
+        // soundTitleAnimation();
+        function soundTitleAnimation(index = 0) {
+
+            const title = document.querySelectorAll(".sound-txtbx-title");
+            const titleWrapper = document.querySelectorAll(".sound-txtbx-title-wrapper");
+
+            const titlerWidth = title[index].clientWidth;
+            const titleWrapperWidth = titleWrapper[index].scrollWidth;
+
+            const distance = titlerWidth + titleWrapperWidth;
+
+            const speed = 100;
+            const duration = distance / speed;
+
+            gsap.to(titleWrapper[index], {
+                x: -distance,
+                ease: "none",
+                duration: duration,
+                repeat: -1,
+                repeatDelay: 0,
+            });
+        }
+
+        const imgbxs = document.querySelectorAll(".sound .swiper-slide .sound-imgbx");
+        const soundAlbums = document.querySelectorAll(".sound .swiper-slide .sound-imgbx-album");
+        const soundTracks = document.querySelectorAll(".sound .swiper-slide .sound-imgbx-track");
+        const soundTexts = document.querySelectorAll(".sound .swiper-slide .sound-txtbx");
+
+        const swiperOptions = {
+            slidesPerView: 1,
+            spaceBetween: 16,
+            pagination: {
+                el: ".sound-swiper-controller .swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            touchable: true,
+            // loop: true,
+            on: {
+                init: function () {
+                    imgbxs[0].classList.add("active");
+                    soundAlbums[0].classList.add("active");
+                    soundTracks[0].classList.add("active");
+                    soundTexts[0].classList.add("active");
+                    soundTitleAnimation(0);
+                },
+                slideChange: function () {
+                    const index_currentSlide = this.realIndex;
+                    const currentSlide = this.slides[index_currentSlide];
+
+                    imgbxs.forEach(el => el.classList.remove("active"));
+                    soundAlbums.forEach(el => el.classList.remove("active"));
+                    soundTracks.forEach(el => el.classList.remove("active"));
+                    soundTexts.forEach(el => el.classList.remove("active"));
+
+
+                    if (currentSlide) {
+                        imgbxs[index_currentSlide].classList.add("active");
+                        soundAlbums[index_currentSlide].classList.add("active");
+                        soundTracks[index_currentSlide].classList.add("active");
+                        soundTexts[index_currentSlide].classList.add("active");
+                        soundTitleAnimation(index_currentSlide);
+                    }
+                }
+            },
+            breakpoints: {
+                769: {
+                    slidesPerView: 2.2,
+                    centeredSlides: true,
+                    // spaceBetween: 100,
+                },
+                1441: {
+                    slidesPerView: 3.5,
+                    centeredSlides: true,
+                    // spaceBetween: 122,
+                }
+            },
+        }
+
+        const swiperInstance = new Swiper(soundSwiper, swiperOptions);
+    }
