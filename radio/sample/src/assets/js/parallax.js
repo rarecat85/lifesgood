1ed77e569fc3121167c7bad5c7664ed810ffe987
sj.concentrix <61546301+dope0515@users.noreply.github.com>
2025-06-11 15:18:05 +0900

    ✨ MOD : parallax 이미지 추가

--- radio/sample/src/assets/js/parallax.js
+++ radio/sample/src/assets/js/parallax.js
@@ -3,7 +3,6 @@ gsap.registerPlugin(ScrollTrigger);
 function parallax() {
   const parallaxContainer = document.querySelector('.parallax-container');
   const parallaxItems = gsap.utils.toArray('.parallax-item');
-  const parallaxTxt = document.querySelector('.parallax-txt-bx');
 
   const parallax = gsap.timeline({
     scrollTrigger:{
@@ -15,7 +14,6 @@ function parallax() {
   });
 
   parallax
-  .to(parallaxTxt, {y: "20%"})
   .to(parallaxItems[0], {y: "-8%"})
   .to(parallaxItems[1], {y: "-24%", x: "-2%"})
   .to(parallaxItems[2], {y: "-10%"});
