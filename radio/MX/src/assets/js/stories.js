86b2c28948660d538af3e68599542530dda1badb
sj.concentrix <61546301+dope0515@users.noreply.github.com>
2025-08-29 10:49:43 +0900

    📝 MOD : OYF(MX)
    - 스크립트 수정

--- radio/MX/src/assets/js/stories.js
+++ radio/MX/src/assets/js/stories.js
@@ -1,24 +1,32 @@
-const storiesSwiper = document.querySelector(".stories .swiper");
+// Stories Swiper 초기화 함수
+function initStories() {
+  const storiesSwiper = document.querySelector(".stories .swiper");
 
-if (storiesSwiper) {
-  const swiperOptions = {
-    slidesPerView: 1.2,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-btnbx-next",
-      prevEl: ".swiper-btnbx-prev",
-    },
-    breakpoints: {
-      769: {
-        slidesPerView: 3,
-        spaceBetween: 16,
+  if (storiesSwiper) {
+    const swiperOptions = {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-btnbx-next",
+        prevEl: ".swiper-btnbx-prev",
       },
-      1441: {
-        slidesPerView: 3,
-        spaceBetween: 24,
-      }
-    },
-  }
+      breakpoints: {
+        769: {
+          slidesPerView: 3,
+          spaceBetween: 16,
+        },
+        1441: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        }
+      },
+    }
 
-  const swiperInstance = new Swiper(storiesSwiper, swiperOptions);
+    const swiperInstance = new Swiper(storiesSwiper, swiperOptions);
+  }
 }
+
+// 모든 리소스가 로드된 후 실행
+window.addEventListener("load", () => {
+  initStories();
+});
